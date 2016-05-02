32b713fb39d2be642876439adb676e2bcf672370
nazopo <npoag@utexas.edu>
2016-05-01 20:23:28 -0500

    First quick and dirty working version.

--- basic_driver/ViewController.swift
+++ basic_driver/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    @IBOutlet var activityState: UILabel!
+    let activityManager = CMMotionActivityManager()
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+            if(CMMotionActivityManager.isActivityAvailable()){
+            self.activityManager.startActivityUpdatesToQueue(NSOperationQueue.mainQueue()){ data in if let data = data {
+                dispatch_async(dispatch_get_main_queue()) {
+                    if(data.stationary == true){
+                        self.activityState.text = "Stationary"
+                        
+                    } else if (data.walking == true){
+                        self.activityState.text = "Walking"
+                        
+                    } else if (data.running == true){
+                        self.activityState.text = "Running"
+                        
+                    } else if (data.automotive == true){
+                        self.activityState.text = "Automotive"
+                    }
+                }
+                
+                }
+            }
+        }
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
